768127ac28865bba2762fc3299ea8530bb0fe266
MarikaKononen <marika.kononen@koodielamastani.fi>
2019-08-09 12:13:35 +0300

    Database added to routes

--- app.js
+++ app.js
@@ -1,36 +1,91 @@
-let express = require("express");
-let app = express();
-let bodyParser = require("body-parser"); 
+let express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser"),
+    mongoose   = require("mongoose");
+ 
+var ObjectId = require('mongodb').ObjectID;
 
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-let campgrounds = [
-    {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60"},
-    {name: "Granite Hill", image: "https://images.unsplash.com/photo-1505232070786-2f46d15f9f5e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60"},
-    {name: "Mountain Goat's Rest", image: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60"}
-]
+// SCHEMA SETUP
+let campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String, 
+    description: String
+});
+
+let Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: "Granite Hill",
+//         image: "https://images.unsplash.com/photo-1559521783-1d1599583485?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60",
+//         description: "This is a huge granite hill, no bathrooms, no water."
+//     },
+//     function(err, campground){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log("Newly created campground: ");
+//             console.log(campground);
+//         }
+//     }
+
+// );
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
+// INDEX route - show all campgrounds
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds})
+    // Get all campgrounds
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index", {campgrounds: allCampgrounds})
+        }
+    });
 });
 
+// CREATE route - add new campground to DB
 app.post("/campgrounds", function(req, res){
-    // get data from form and add to campgrounds array
+    // get data from form 
     let name = req.body.name;
     let image = req.body.image;
-    var newCampground = {name: name, image:image}
-    campgrounds.push(newCampground);
-    // redirect back to campgrounds page
-    res.redirect("/campgrounds");
+    let desc = req.body.description;
+    var newCampground = {name: name, image:image, description:desc}
+    // Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            // redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
+// NEW route - show for to create new campground
 app.get("/campgrounds/new", function(req, res){
     res.render("new.ejs")
+});
+
+// SHOW route - shows info about one campground
+app.get("/campgrounds/:id", function(req, res){
+    // find the campground with provided ID
+    console.log('show route ',req.params.id)
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            // render show template with that campground
+            res.render("show", {campground: foundCampground});
+        }
+    });
 });
 
 app.listen(3000, function(){
