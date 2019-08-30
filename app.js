a7ef0c9584cf6813ad1169e2446e3094b8259d0c
MarikaKononen <marika.kononen@koodielamastani.fi>
2019-08-30 12:47:24 +0300

    add styles to app

--- app.js
+++ app.js
@@ -61,6 +61,7 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 
 
 var port = process.env.PORT || 3000;
+
 app.listen(port, function(){
     console.log("The YelpCamp Server has started!");
 });
